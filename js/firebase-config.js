928008b4059030bc0a7878f0c3faa6638a92330d
CromiChan_ <106527065+CrockYT@users.noreply.github.com>
2024-11-25 12:27:22 +0900

    Delete js directory

--- js/firebase-config.js
+++
@@ -1,6 +0,0 @@
-export async function getFirebaseConfig() {
-    const response = await fetch('https://ser.cromichan-net.workers.dev/', { method: 'GET' });
-    const config = await response.json();
-    return config;
-  }
-  
--- js/firebase-init.js
+++
@@ -1,9 +0,0 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js';
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js';
-import { getFirebaseConfig } from './firebase-config.js';
-
-export async function initFirebase() {
-  const config = await getFirebaseConfig();
-  const app = initializeApp(config);
-  return getFirestore(app);
-}
